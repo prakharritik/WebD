6da28a9c8c975f9afe16e41c1e1db15cf8b645df
prakharritik <prakharritik@users.noreply.github.com>
2020-05-25 12:06:26 +0530

    u

--- sol5/js/script.js
+++ sol5/js/script.js
@@ -114,7 +114,8 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-       var homeHtmlToInsertIntoMainPage =  "'" + chosenCategoryShortName + "'";
+      chosenCategoryShortName =  "'" + chosenCategoryShortName + "'";
+       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
